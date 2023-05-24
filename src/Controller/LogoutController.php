d60d7f3ee2e218314866518d5752a33ed3915dc7
jngermon <jn.germon@gmail.com>
2023-05-24 22:09:54 +0200

    Add logout controller

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Code202\Security\Controller;
+
+use Nelmio\ApiDocBundle\Annotation\Security as OASecurity;
+use OpenApi\Attributes as OA;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[OA\Tag(name: ' Logout')]
+#[Route('/logout', name: '.logout')]
+#[OASecurity(name: null)]
+class LogoutController
+{
+    #[OA\Post(
+        responses: [
+            new OA\Response(
+                response: '204',
+                description: 'Successful',
+                content: new OA\JsonContent(
+                    ref: '#/components/schemas/LoginResponse'
+                )
+            )
+        ]
+    )]
+    #[Route('', name: '', methods: 'POST')]
+    public function logout(Security $security): Response
+    {
+        $security->logout();
+
+        return new Response(null, 204, [], true);
+    }
+}
