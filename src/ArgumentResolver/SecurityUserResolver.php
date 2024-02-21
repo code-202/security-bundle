cc9accd541d8cad37ced626b3e9526c62afdcb8c
jngermon <jn.germon@gmail.com>
2024-02-21 22:50:28 +0100

    Fix SecurityUserResolver bad compatibility to SF 7.0

--- src/ArgumentResolver/SecurityUserResolver.php
+++ src/ArgumentResolver/SecurityUserResolver.php
@@ -2,13 +2,14 @@
 
 namespace Code202\Security\ArgumentResolver;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
-use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use Code202\Security\User\UserInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-class SecurityUserResolver implements ArgumentValueResolverInterface
+class SecurityUserResolver implements ValueResolverInterface
 {
     protected TokenStorageInterface $tokenStorage;
 
@@ -18,17 +19,18 @@ class SecurityUserResolver implements ArgumentValueResolverInterface
         $this->tokenStorage = $tokenStorage;
     }
 
-    public function supports(Request $request, ArgumentMetadata $argument): bool
-    {
-        if ($argument->getType() !== UserInterface::class) {
-            return false;
-        }
-
-        return $this->tokenStorage->getToken()?->getUser() instanceof UserInterface;
-    }
-
     public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
-        yield $this->tokenStorage->getToken()->getUser();
+        if ($argument->getType() !== UserInterface::class) {
+            return [];
+        }
+
+        $user = $this->tokenStorage->getToken()?->getUser();
+
+        if ($user instanceof UserInterface) {
+            return [$user];
+        }
+
+        throw new AccessDeniedException(sprintf('The logged-in user is an instance of "%s" but a user of type "%s" is expected.', $user::class, $argument->getType()));
     }
 }
